04721771f70e5119d1fc15388fb0df32e32c2824
zengchanghuan <zengchanghuan@gmail.com>
2016-06-01 18:22:38 +0800

    提交事件处理

--- weibo/Cube/Classes/home/Model/XZSpecial.h
+++ weibo/Cube/Classes/home/Model/XZSpecial.h
@@ -19,4 +19,9 @@
  *  特殊文字的范围
  */
 @property (nonatomic, assign) NSRange range;
+
+/**
+ *  这段特殊文字的矩形框(要求数组里面存放CGRect)
+ */
+@property (nonatomic, strong) NSArray *rects;
 @end
--- weibo/Cube/Classes/home/View/XZStatusTextView.m
+++ weibo/Cube/Classes/home/View/XZStatusTextView.m
@@ -24,6 +24,44 @@
     }
     return self;
 }
+- (void)setupSpecialRects
+{
+    NSArray *specials = [self.attributedText attribute:@"specials" atIndex:0 effectiveRange:NULL];
+    for (XZSpecial *special in specials) {
+        self.selectedRange = special.range;
+        // self.selectedRange --影响--> self.selectedTextRange
+        // 获得选中范围的矩形框
+        NSArray *selectionRects = [self selectionRectsForRange:self.selectedTextRange];
+        // 清空选中范围
+        self.selectedRange = NSMakeRange(0, 0);
+        
+        NSMutableArray *rects = [NSMutableArray array];
+        for (UITextSelectionRect *selectionRect in selectionRects) {
+            CGRect rect = selectionRect.rect;
+            if (rect.size.width == 0 || rect.size.height == 0) continue;
+            
+            // 添加rect
+            [rects addObject:[NSValue valueWithCGRect:rect]];
+        }
+        special.rects = rects;
+    }
+}
+/**
+ 找出被触摸的特殊字符串
+ */
+- (XZSpecial *)touchingSpecialWithPoint:(CGPoint)point
+{
+    NSArray *specials = [self.attributedText attribute:@"specials" atIndex:0 effectiveRange:NULL];
+    for (XZSpecial *special in specials) {
+        for (NSValue *rectValue in special.rects) {
+            if (CGRectContainsPoint(rectValue.CGRectValue, point)) { // 点中了某个特殊字符串
+                return special;
+            }
+        }
+    }
+    return nil;
+}
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     // 触摸对象
@@ -32,45 +70,22 @@
     // 触摸点
     CGPoint point = [touch locationInView:self];
     
-    NSArray *specials = [self.attributedText attribute:@"specials" atIndex:0 effectiveRange:NULL];
-    BOOL contains = NO;
+    // 初始化矩形框
+    [self setupSpecialRects];
     
-    for (XZSpecial *special in specials) {
-        self.selectedRange = special.range;
-        // self.selectedRange --影响--> self.selectedTextRange
-        // 获得选中范围的矩形框
-        NSArray *rects = [self selectionRectsForRange:self.selectedTextRange];
-        // 清空选中范围
-        self.selectedRange = NSMakeRange(0, 0);
-        
-        for (UITextSelectionRect *selectionRect in rects) {
-            CGRect rect = selectionRect.rect;
-            if (rect.size.width == 0 || rect.size.height == 0) continue;
-            
-            if (CGRectContainsPoint(rect, point)) { // 点中了某个特殊字符串
-                contains = YES;
-                break;
-            }
-        }
-        
-        if (contains) {
-            for (UITextSelectionRect *selectionRect in rects) {
-                CGRect rect = selectionRect.rect;
-                if (rect.size.width == 0 || rect.size.height == 0) continue;
-                
-                UIView *cover = [[UIView alloc] init];
-                cover.backgroundColor = [UIColor greenColor];
-                cover.frame = rect;
-                cover.tag = XZStatusTextViewCoverTag;
-                cover.layer.cornerRadius = 5;
-                [self insertSubview:cover atIndex:0];
-            }
-            
-            break;
-        }
-    }
+    // 根据触摸点获得被触摸的特殊字符串
+    XZSpecial *special = [self touchingSpecialWithPoint:point];
     
     // 在被触摸的特殊字符串后面显示一段高亮的背景
+    for (NSValue *rectValue in special.rects) {
+        // 在被触摸的特殊字符串后面显示一段高亮的背景
+        UIView *cover = [[UIView alloc] init];
+        cover.backgroundColor = [UIColor greenColor];
+        cover.frame = rectValue.CGRectValue;
+        cover.tag = XZStatusTextViewCoverTag;
+        cover.layer.cornerRadius = 5;
+        [self insertSubview:cover atIndex:0];
+    }
 }
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
@@ -83,6 +98,23 @@
     // 去掉特殊字符串后面的高亮背景
     for (UIView *child in self.subviews) {
         if (child.tag == XZStatusTextViewCoverTag) [child removeFromSuperview];
+    }
+}
+/**
+ 告诉系统:触摸点point是否在这个UI控件身上
+ */
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event
+{
+    // 初始化矩形框
+    [self setupSpecialRects];
+    
+    // 根据触摸点获得被触摸的特殊字符串
+    XZSpecial *special = [self touchingSpecialWithPoint:point];
+    
+    if (special) {
+        return YES;
+    } else {
+        return NO;
     }
 }
 
